f871b31e92c122e24e0d73b1c5c4d34b972613b8
Ricardo Montoro <Ricardo.montorogonzalez@outlook.com>
2021-09-04 00:17:02 -0700

    Cambios en dashboard fragment

--- app/src/main/java/com/montororg/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/montororg/myapplication/ui/home/HomeFragment.java
@@ -1,24 +1,32 @@
 package com.montororg.myapplication.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.montororg.myapplication.R;
 import com.montororg.myapplication.databinding.FragmentHomeBinding;
+import com.montororg.myapplication.ui.dashboard.DashboardFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private TextView miTexto;
+    private LinearLayout linearLayout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,16 +34,38 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View miVista = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        miTexto = miVista.findViewById(R.id.texto);
+        cambiarTexto("Texto de inicio en la app");
+
+        ImageView botoncito = miVista.findViewById(R.id.imagen2);
+        botoncito.setOnClickListener(myListener);
+
+        linearLayout = miVista.findViewById(R.id.linearlayout2);
+
+
+
+
+
+        return miVista;
+    }
+
+    private View.OnClickListener myListener = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            /*
+            FragmentManager fragmentManager = getParentFragmentManager();
+            Fragment dashboard = new DashboardFragment();
+
+            fragmentManager.beginTransaction().replace(linearLayout.getId(), dashboard).commitNow();*/
+
+
+        }
+    };
+
+    private void cambiarTexto(String texto){
+        miTexto.setText(texto);
     }
 
     @Override
